32c4c2bd84a208948156ad379f21dbda547538e8
vzlfkr <muhamad.zulfikar.official@gmail.com>
2024-10-21 01:45:32 +0800

    Update setup.bat
    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,17 +1,20 @@
 @echo off
+setlocal
+set current_dir=%~dp0
+
 echo Starting setup...
 
 echo Copying .env file...
-call copy-env.bat
+call "%current_dir%copy-env.bat"
 
 echo Running backend install...
-call install-backend.bat
+call "%current_dir%install-backend.bat"
 
 echo Running database migration...
-call migrate-database.bat
+call "%current_dir%migrate-database.bat"
 
 echo Running frontend install...
-call install-frontend.bat
+call "%current_dir%install-frontend.bat"
 
 echo Setup complete! Now you can run the web app using start-webapp.bat
 pause
